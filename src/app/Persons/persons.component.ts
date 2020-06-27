23712ec5392899ac868ca06e3d75fa83a796da6b
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-27 17:44:49 +0200

    Input decorator + basic structural directives

--- src/app/Persons/persons.component.ts
+++ src/app/Persons/persons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-persons',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
 })
 
 export class PersonsComponent {
+    @Input() personList: string[];
+
+
 
 }
