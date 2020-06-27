4f6ea4869d24c231b6a286b544efed56eb953b07
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-27 19:08:23 +0200

    template refrence

--- src/app/Persons/person-input.component.ts
+++ src/app/Persons/person-input.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 
 export class PersonInputComponent {
 
+    onCreatePerson(personName: string) {
+        console.log('created a person ' + personName)
+    }
+
+
 }
