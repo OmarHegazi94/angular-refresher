f7644bdf65b73e8f1a494491bf6db7d5d29aedf8
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2020-06-28 13:35:51 +0200

    rxjs subjects

--- src/app/Persons/persons.component.ts
+++ src/app/Persons/persons.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-persons',
     templateUrl: './persons.component.html'
 })
 
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
     // @Input() personList: string[];
     // private personService: PersonsService;
     personList: string[];
+
+    private personListSubs: Subscription;
 
     constructor(private personSrv: PersonsService){
         // this.personList = personSrv.persons;
@@ -18,6 +21,17 @@ export class PersonsComponent implements OnInit {
 
     ngOnInit() {
         this.personList = this.personSrv.persons;
+        this.personListSubs = this.personSrv.personsChanged.subscribe( persons => {
+            this.personList = persons;
+        });
+    }
+
+    onRemovePerson(personName: string) {
+        this.personSrv.removePerson(personName);
+    }
+
+    ngOnDestroy(){
+        this.personListSubs.unsubscribe();
     }
 
 }
